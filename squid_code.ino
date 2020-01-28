23aa0ab179d807cbf46dcab7aa6e5df66c3fafd1
colinauyeung <35784840+colinauyeung@users.noreply.github.com>
2020-01-28 15:15:38 -0700

    Added Comments

--- squid_code.ino
+++ squid_code.ino
@@ -26,7 +26,7 @@ int b_pin = 5;            //Blue pin o RGB LED
 long duration, cm, inches;  //values for HC-SR04
 int button_in;              //Button output
 int button_in_last = 0;     //Last button output
-bool on = true;             //If the system is on
+bool on = true;             //If the system is on or not
 
 
 
@@ -39,10 +39,67 @@ void setColor(int redValue, int greenValue, int blueValue) {
 }
 
 
-//From: https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04/
-//Gets the distance from the UltraSonic sensor
-void getUltraSonic(){      
+//Set Up Code
+void setup()
+{
+  //Begin Serial Output
+  Serial.begin(9600);
 
+  //Set up Pin to correct modes
+  pinMode(led_pin, OUTPUT);
+  pinMode(led2_pin, OUTPUT);
+  
+  pinMode(digitalPin, INPUT);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(button, INPUT);
+  
+  pinMode(r_pin, OUTPUT);
+  pinMode(g_pin, OUTPUT);
+  pinMode(b_pin, OUTPUT);
+
+  setColor(0, 0, 255); // Set to Blue
+}
+
+
+void loop()
+{
+
+  //Read Button
+  button_in = digitalRead(button);
+
+  //Check if the button has been hit
+  if(button_in_last == 0 and button_in == 1){
+
+    //If system is on, turn off
+    if(on){
+      on = false;
+      digitalWrite(led_pin, LOW);
+      digitalWrite(led2_pin, LOW);
+      setColor(0,0,0);
+    }
+
+    //Else turn system on
+    else{
+      on = true;
+    }
+  }
+
+  //Reset last button input
+  button_in_last = button_in;
+
+  //If system is on
+  if(on){
+
+    //Turn on bottom leds
+    digitalWrite(led_pin, HIGH);
+    digitalWrite(led2_pin, HIGH);
+
+
+    //From: https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04/
+    
     // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
     // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
     digitalWrite(trigPin, LOW);
@@ -61,76 +118,45 @@ void getUltraSonic(){
     // Convert the time into a distance
     cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
     inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-}
-
-
-//Set Up Code
-void setup()
-{
-
-  Serial.begin(9600);
-  
-  pinMode(led_pin, OUTPUT);
-  pinMode(led2_pin, OUTPUT);
-  
-  pinMode(digitalPin, INPUT);
-
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  pinMode(button, INPUT);
-  
-  pinMode(r_pin, OUTPUT);
-  pinMode(g_pin, OUTPUT);
-  pinMode(b_pin, OUTPUT);
-
-  setColor(0, 0, 255); // Purple Color 
-}
-
-
-void loop()
-{
-
-  
-  button_in = digitalRead(button);
-  if(button_in_last == 0 and button_in == 1){
-    if(on){
-      on = false;
-    }
-    else{
-      on = true;
-    }
-  }
-
-  
-  button_in_last = button_in;
-
-  if(on){
-    digitalWrite(led_pin, HIGH);
-    digitalWrite(led2_pin, HIGH);
-
-
-    getUtlraSonic();
     
+    //End of code from https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04/
+
+    //Read value from reed sensor
     digitalVal = digitalRead(digitalPin);
-  
-   
+    Serial.print(digitalVal);
+
+    //If something is in front of user
     if(inches < 5){
+
+      //And reed sensor detects something
       if(digitalVal == HIGH){
+
+        //change to yellow
         setColor(255, 255, 0);
       }
       else{
+
+        //Otherwise change to red
         setColor(255, 0 , 0);
       }
     }
+
+    //If nothing is in front of the user
     else{
+
+      //If the reed detects something
       if(digitalVal == HIGH){
+
+        //Change to green
         setColor(0, 255, 0);
       }
       else{
+        //If nothing is happening, change to blue
         setColor(0, 0, 255); 
       }
     }
   }
+
+  //Wait for next iteration
   delay(100);
 }
